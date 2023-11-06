2932d0eb9a6113a7718f17db7e689e5434a7d59e
evan <evandromariano49@gmail.com>
2023-11-06 18:00:30 -0300

    projeto2 finalizado

--- projeto2/Program.cs
+++ projeto2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using projeto2.Models;
+
+Console.WriteLine("Evandro José da Silva Mariano");
+Console.WriteLine(DateTime.Now);
+
+Pessoa p = new Pessoa();
+p.Nome = "Evandro Mariano";
+p.Idade = 25;
+p.apresentar();
