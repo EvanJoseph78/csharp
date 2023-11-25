5a15fd006ecc7057289a82db20ac4968edf02670
Evan Joseph <evandromariano49@gmail.com>
2023-11-25 20:28:26 -0300

    csharp

---
+++ vetores/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// string[] listaNomes = new string[10];
+// listaNomes[0] = "Evandro";
+//
+// Console.WriteLine(listaNomes[0]);
+//
+
+//
